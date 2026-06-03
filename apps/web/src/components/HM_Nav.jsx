f72c24bee91ddd08b28de1fcbcd07313db8dc574
Nydiliria <sander2002@gmail.com>
2026-06-03 10:27:25 +0200

    hotfix

--- apps/web/src/components/HM_Nav.jsx
+++ apps/web/src/components/HM_Nav.jsx
@@ -22,7 +22,8 @@ export default function HM_Nav(){
 
                 <nav className="nav-menu">
                     {/* Command Center link */}
-                    <Link to="/commando_centrum" className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
+                    <Link to="/commando_centrum"
+                          className={`nav-item ${location.pathname === '/command_centrum' ? 'active' : ''}`}>
                         <svg className="nav-icon" viewBox="0 0 24 24">
                             <path fill="currentColor"
                                   d="M4 13h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zm0 8h6c.55 0 1-.45 1-1v-4c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v4c0 .55.45 1 1 1zm10 0h6c.55 0 1-.45 1-1v-8c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1v8c0 .55.45 1 1 1zM14 4v4c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V4c0-.55-.45-1-1-1h-6c-.55 0-1 .45-1 1z"/>
