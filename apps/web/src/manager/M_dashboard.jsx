5c2d752470e1e611ea449cede1c16e9b4bb56fa6
Nydiliria <Nydiliriayt@gmail.com>
2026-06-11 11:30:16 +0200

    fixed the M_dashboard.jsx to not have a weird layout bug anymore, needs some more love

--- apps/web/src/manager/M_dashboard.jsx
+++ apps/web/src/manager/M_dashboard.jsx
@@ -6,17 +6,11 @@ import "./Manager_styling.css"
 
 function M_dashboard() {
     return (
-        <>
+        <div className="dashboard-layout">
             <HM_Nav></HM_Nav>
             <main className="main-content profile-container">
 
                 <header className="profile-user-card">
-                    <div className="profile-avatar-box">
-                        <svg viewBox="0 0 24 24" width="32" height="32" fill="currentColor">
-                            <path
-                                d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.79-7 8.94V12H5V6.3l7-3.11v8.8z"/>
-                        </svg>
-                    </div>
                     <div className="profile-user-info">
                         <h2>Manager</h2>
                         <p className="profile-email">manager@rauw.nl</p>
@@ -133,7 +127,7 @@ function M_dashboard() {
 
 
             </main>
-        </>
+        </div>
     );
 }
 
